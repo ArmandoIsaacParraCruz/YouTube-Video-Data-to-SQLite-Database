d8d0af0ebf086503de9526adf9a94484e7a47156
ArmandoIsaacParraCruz <121985302+ArmandoIsaacParraCruz@users.noreply.github.com>
2023-07-29 12:54:51 -0600

    adding comments

--- main.py
+++ main.py
@@ -5,47 +5,43 @@ import my_database
 
 API_KEY = 'DEVELOPER_KEY'
 
+# Create a YouTube API service
 api_service_name = "youtube"
 api_version = "v3"
 youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=API_KEY)
 
-
+# Prompt the user to enter a search term
 topic = input("Enter the term to search for: ")
 
+# Display order types for sorting the search results
 print('''Order types to sort the list:
-
-date – Resources are sorted in reverse chronological 
-order based on the date they were created.
-		   
+date – Resources are sorted in reverse chronological order based on the date they were created.
 rating – Resources are sorted from highest to lowest rating.
-	
-relevance – Resources are sorted based on their relevance to 
-the search query. This is the default value for this parameter.
-				
+relevance – Resources are sorted based on their relevance to the search query. This is the default value for this parameter.
 title – Resources are sorted alphabetically by title.
-	
 videoCount – Channels are sorted in descending order of their number of uploaded videos.
-	
-viewCount – Resources are sorted from highest to lowest number of views. 
-For live broadcasts, videos are sorted by number of concurrent viewers while 
-the broadcasts are ongoing.
-
+viewCount – Resources are sorted from highest to lowest number of views. For live broadcasts, videos are sorted by the number of concurrent viewers while the broadcasts are ongoing.
 ''')
 
+# Prompt the user to choose the order type for sorting
 order_param = input("Enter order type (date, rating, relevance, title, videoCount, viewCount): ")
+
+# Prompt the user to enter the maximum number of results to retrieve
 max_result = abs(int(input("Enter the number of results you want, max = 50: ")))
 
+# Limit the number of results to a maximum of 50
 if max_result > 50:
     max_result = 50
 
 try:
+    # Perform the YouTube API search with the specified parameters
     query_videos_ids = youtube.search().list(
         part="id",
         type='video',
         regionCode="US",
-        order= order_param,
+        order=order_param,
         q=topic,
-        maxResults= max_result,
+        maxResults=max_result,
         fields="items(id(videoId))"
     ).execute()
 except:
@@ -55,59 +51,57 @@ except:
 print("\nResults of the query:\n")
 count = 0
 
+# Create the database
 my_database.create_database()
 
+# Loop through each video ID in the search results
 for item in query_videos_ids['items']:
     count += 1
     vid_id = item['id']['videoId']
+    
+    # Request video statistics and snippet data
     response = youtube.videos().list(
         part="statistics,snippet",
         id=vid_id
     ).execute()
 
-    #try:
-    # getting the title of the video
-    video_title = response['items'][0]['snippet']['title']
+    try:
+        # Extract video details: title, publish datetime, view count, like count, comment count, video URL, and channel name
+        video_title = response['items'][0]['snippet']['title']
+        published_at = response['items'][0]['snippet']['publishedAt']
+        published_datetime = dateutil.parser.parse(published_at)
+        datetime_iso8601 = published_datetime.strftime('%Y-%m-%d %H:%M:%S')
+        view_count = int(response['items'][0]['statistics']['viewCount'])
+        like_count = int(response['items'][0]['statistics']['likeCount'])
+        comment_count = int(response['items'][0]['statistics']['commentCount'])
+        video_url = f"https://www.youtube.com/watch?v={vid_id}"
+        channel_id = response['items'][0]['snippet']['channelId']
         
-    # getting the publish datetime
-    published_at = response['items'][0]['snippet']['publishedAt']
-    published_datetime = dateutil.parser.parse(published_at)
-    datetime_iso8601 = published_datetime.strftime('%Y-%m-%d %H:%M:%S')
-    
-    # getting the view count
-    view_count = int(response['items'][0]['statistics']['viewCount']) 
-    
-    # getting the like count
-    like_count = int(response['items'][0]['statistics']['likeCount']) 
-    
-    # getting the comment count
-    comment_count = int(response['items'][0]['statistics']['commentCount']) 
-    
-    #getting the url
-    video_url = f"https://www.youtube.com/watch?v={vid_id}"
-    
-    # getting the channel name
-    channel_id = response['items'][0]['snippet']['channelId']
-    channel_response = youtube.channels().list(
-        part='snippet',
-        id=channel_id
-    ).execute()
-    channel_name = channel_response['items'][0]['snippet']['title']
-    
-    my_database.insert_record(video_title, datetime_iso8601, view_count, like_count,
-                 comment_count, video_url, channel_name)
-                 
-    print(count, 'video name:', video_title)
-    print("Channel name:", channel_name)
-    print("Views:", view_count)
-    print("Likes:", like_count)
-    print("#comments:", comment_count)
-    print(video_url)
-    print('Publish datetime:', datetime_iso8601)
-    print()
-    #except:
-    #    continue
+        # Get the channel name associated with the channel ID
+        channel_response = youtube.channels().list(
+            part='snippet',
+            id=channel_id
+        ).execute()
+        channel_name = channel_response['items'][0]['snippet']['title']
+        
+        # Insert the video details into the database
+        my_database.insert_record(video_title, datetime_iso8601, view_count, like_count, comment_count, video_url, channel_name)
+        
+        # Print the details of the video
+        print(count, 'video name:', video_title)
+        print("Channel name:", channel_name)
+        print("Views:", view_count)
+        print("Likes:", like_count)
+        print("#comments:", comment_count)
+        print(video_url)
+        print('Publish datetime:', datetime_iso8601)
+        print()
+    except:
+        # Continue to the next video if an error occurs while processing the current video
+        print("Error processing a video")
+        continue
 
+# Close the database connection
 my_database.close_database()
 
 
--- my_database.py
+++ my_database.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-
 conn = sqlite3.connect('videos.sqlite')
 cur = conn.cursor()
 
 def create_database():
+    # Create the 'Videos' and 'Channels' tables in the database
     cur.executescript('''
     DROP TABLE IF EXISTS Videos;
     DROP TABLE IF EXISTS Channels;
@@ -20,30 +20,31 @@ def create_database():
         channel_id INTEGER
     );
 
-
     CREATE TABLE Channels (
         id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
         name   TEXT UNIQUE
     )
     ''')
     
-def insert_record(title, publish_datetime, view_count, like_count,
-                     comment_count, url, channel_name):
-    
-    # inserting a new record in channel's table
+def insert_record(title, publish_datetime, view_count, like_count, comment_count, url, channel_name):
+    # Insert a new record in the 'Channels' table, ignore if the channel name already exists
     cur.execute('''INSERT OR IGNORE INTO Channels (name)
-        VALUES ( ? )''', ( channel_name, ) )
-    cur.execute('SELECT id FROM Channels WHERE name = ? ', (channel_name, ))
+        VALUES ( ? )''', (channel_name,))
+    # Get the channel ID associated with the channel name
+    cur.execute('SELECT id FROM Channels WHERE name = ? ', (channel_name,))
     channel_id = cur.fetchone()[0]
     
+    # Insert a new record in the 'Videos' table
     cur.execute('''INSERT OR IGNORE INTO Videos 
         (title, publish_datetime, view_count, like_count, comment_count, url, channel_id)
         VALUES ( ?, ?, ?, ?, ?, ?, ? )''', 
-        (title, publish_datetime, view_count, like_count, comment_count, url, channel_id) )
- 
+        (title, publish_datetime, view_count, like_count, comment_count, url, channel_id))
+    
+    # Commit the changes to the database
     conn.commit()
 
 def close_database():
+    # Close the database connection
     conn.close()   
-    
+
  
